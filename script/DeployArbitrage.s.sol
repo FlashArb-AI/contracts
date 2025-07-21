7a7001be0ac81240078a1194547c371f5e0ccf42
Riiz0 <shawnrizo@hotmail.com>
2025-07-21 12:21:30 -0400

    Feat: Add Full Detailed Natspec & Add MIT License To DeploymentArbitrage Script

--- script/DeployArbitrage.s.sol
+++ script/DeployArbitrage.s.sol
@@ -1,10 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {FlashArbitrage} from "../src/FlashArbitrage.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
+/**
+ * @title DeployArbitrage
+ * @author FlashArbAI
+ * @notice This contract is responsible for deploying the FlashArbitrage contract
+ * @dev Deployment script that uses Foundry's Script contract for deploying FlashArbitrage
+ *      The script configures network settings using HelperConfig and deploys the arbitrage contract
+ *      with proper broadcasting for mainnet/testnet deployment
+ */
 contract DeployArbitrage is Script {
     HelperConfig public helperConfig;
     HelperConfig.NetworkConfig modeConfig;
