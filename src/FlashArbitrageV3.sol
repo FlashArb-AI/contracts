30cc038eba17002c82bd5d7e576522ede5ba8005
Shawn Rizo <shawnrizo@hotmail.com>
2025-08-18 00:19:34 -0400

    Feat: Add private constant MAX_BPS Var

--- src/FlashArbitrageV3.sol
+++ src/FlashArbitrageV3.sol
@@ -42,4 +42,6 @@ contract ImprovedFlashArbitrageV3 is IFlashLoanRecipient, ReentrancyGuard, Ownab
 
     /// @notice Balancer V2 Vault address for flash loans
     IVault private constant VAULT = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+
+    uint256 private constant MAX_BPS = 10000;
 }
