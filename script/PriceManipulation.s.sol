94a41a22f058a5d9cf7ce0dc47078b8730e87b6a
malgus01 <mlghectiik@gmail.com>
2025-09-28 10:51:40 -0700

    feat: Refactor Swap.s.sol Script

--- script/PriceManipulation.s.sol
+++ script/PriceManipulation.s.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {IERC20} from "@openzeppelin/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import {IQuoterV2} from "@uniswap/v3-periphery/contracts/interfaces/IQuoterV2.sol";
 
